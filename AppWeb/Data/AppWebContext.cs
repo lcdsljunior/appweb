d69a5ff11a08d1fe10e2514efcaaa16326800960
luiz.carlos <luizjunior.ti@gmail.com>
2023-09-15 15:37:42 -0300

    Crud scaffolding

---
+++ AppWeb/Data/AppWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppWeb.Models;
+
+namespace AppWeb.Data
+{
+    public class AppWebContext : DbContext
+    {
+        public AppWebContext (DbContextOptions<AppWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppWeb.Models.Department> Department { get; set; } = default!;
+    }
+}
