fc29729814d2ee7aa963952e85ea57759ba498c4
luiz.carlos <luizjunior.ti@gmail.com>
2023-09-15 15:19:07 -0300

    first commit

---
+++ AppWeb/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using AppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "CTO" });
+            list.Add(new Department { Id = 2, Name = "CRC" });
+            list.Add(new Department { Id = 3, Name = "ADMINISTRAÇÂO" });
+
+            return View(list);
+        }
+    }
+}
---
+++ AppWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using AppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Email"] = "luiz.carlos@nubus.com.br";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new AppWeb.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
